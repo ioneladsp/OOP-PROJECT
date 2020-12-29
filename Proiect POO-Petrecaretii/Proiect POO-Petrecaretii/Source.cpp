ace6e8a7960203caf8bcec4d8801375df6dfbf5d
alexandrucracea <alexandru.cracea29@gmail.com>
2020-12-29 19:03:38 +0200

    Create Source.cpp

---
+++ Proiect POO-Petrecaretii/Proiect POO-Petrecaretii/Source.cpp
@@ -0,0 +1,20 @@
+//MAIN:
+#pragma warning(disable:4996)
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include<ctype.h>
+#include"Header.h"
+using namespace std;
+
+int main()
+{
+	system("title Proiect POO");
+	system("cls");
+	system("color 8F");
+	meniu a;
+	a.verifica();
+	cout << endl;
+}
