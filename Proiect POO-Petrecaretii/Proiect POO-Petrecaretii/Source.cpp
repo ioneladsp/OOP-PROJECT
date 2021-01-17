a416717770a415c9651b42df559a45920420bd80
ioneladsp <ionela.despa16@gmail.com>
2021-01-17 16:06:25 +0200

    Update Source.cpp

--- Proiect POO-Petrecaretii/Proiect POO-Petrecaretii/Source.cpp
+++ Proiect POO-Petrecaretii/Proiect POO-Petrecaretii/Source.cpp
@@ -1,20 +1,32 @@
-//MAIN:
 #pragma warning(disable:4996)
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <fstream>
 #include <vector>
-#include<ctype.h>
-#include"Header.h"
+#include <ctype.h>
+#include "Header.h"
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	system("title Proiect POO");
-	system("cls");
-	system("color 8F");
-	meniu a;
-	a.verifica();
+	cout << "Numar parametri (maxim 5) = " << argc << endl;
+	if (argc <= 5)
+	{
+		for (int i = 0; i < argc; i++)
+		{
+			cout << argv[i] << endl;
+		}
+		system("title Proiect POO");
+		system("cls");
+		system("color 8F");
+		meniu a;
+		a.verifica();
+	}
+	else
+	{
+		cout << " Numarul de parametri introdusi este prea mare." << endl;
+	}
+	cout << " Multumim!";
 	cout << endl;
 }
